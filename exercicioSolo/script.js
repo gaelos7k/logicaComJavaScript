88d416b0e3114002c3c58e7daa02f1b24460fcff
unknown <l77gabrielgh77@hotmail.com>
2025-01-08 21:07:17 -0300

    Mais um número secreto para testar a lógica.

--- exercicioSolo/script.js
+++ exercicioSolo/script.js
@@ -6,17 +6,21 @@ const dificuldade = 10;
 console.log(`Para fins de teste o número secreto é ${numeroSecreto}.`);
 
 let numeroEscolhido;
+let chute = 1;
 
 while (numeroEscolhido != numeroSecreto) {
+
+    let palavraTentativa = chute === 1 ? 'tentativa.' : 'tentativas.';
 
     numeroEscolhido = parseInt(prompt(`Escolha um número entre 0 e ${dificuldade}`));
 
     if (numeroEscolhido === numeroSecreto) {
-        alert('Parábens você acertou.');
+        alert(`Parábens você acertou com ${chute} ${palavraTentativa}`);
         break;
     } else if (numeroEscolhido > numeroSecreto) {
         alert(`Tente novamente! O número secreto é menor que ${numeroEscolhido}.`);
     } else {
         alert(`Tente novamente! O número secreto é maior que ${numeroEscolhido}`);
     }
+    chute++;
 }
