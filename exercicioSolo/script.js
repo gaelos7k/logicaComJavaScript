f53cf3174b78a9b0a8d4a482816c6ee685386703
unknown <l77gabrielgh77@hotmail.com>
2025-01-09 16:05:35 -0300

    validação da entrada do usuário.

--- exercicioSolo/script.js
+++ exercicioSolo/script.js
@@ -1,31 +1,35 @@
-alert('Bem vindo ao meu jogo do número secreto.');
+function reiniciar() {
+    alert('Bem vindo ao meu jogo do número secreto.');
 
-const numeroSecreto = parseInt(Math.random() * 11);
-const dificuldade = 10;
+    const numeroSecreto = parseInt(Math.random() * 11);
+    const dificuldade = 10;
 
-console.log(`Para fins de teste o número secreto é ${numeroSecreto}.`);
+    console.log(`Para fins de teste o número secreto é ${numeroSecreto}.`);
 
-let numeroEscolhido;
-let chute = 1;
+    let numeroEscolhido;
+    let chute = 1;
 
-while (numeroEscolhido != numeroSecreto) {
+    while (numeroEscolhido != numeroSecreto) {
 
-    let palavraTentativa = chute === 1 ? 'tentativa.' : 'tentativas.';
+        let palavraTentativa = chute === 1 ? 'tentativa.' : 'tentativas.';
 
-    numeroEscolhido = parseInt(prompt(`Escolha um número entre 0 e ${dificuldade}`));
+        numeroEscolhido = parseInt(prompt(`Escolha um número entre 0 e ${dificuldade}`));
 
-    while (isNaN(numeroEscolhido) === true) {
-        numeroEscolhido = parseInt(prompt(`Escolha um número entre 0 e ${dificuldade} que seja válido!`));
+        while (isNaN(numeroEscolhido) === true) {
+            numeroEscolhido = parseInt(prompt(`Escolha um número entre 0 e ${dificuldade} que seja válido!`));
+        }
+
+        if (numeroEscolhido === numeroSecreto) {
+            alert(`Parábens você acertou com ${chute} ${palavraTentativa}`);
+            break;
+        } else if (numeroEscolhido > numeroSecreto) {
+            alert(`Tente novamente! O número secreto é menor que ${numeroEscolhido}.`);
+        } else {
+            alert(`Tente novamente! O número secreto é maior que ${numeroEscolhido}`);
+        }
+
+        chute++;
     }
-
-    if (numeroEscolhido === numeroSecreto) {
-        alert(`Parábens você acertou com ${chute} ${palavraTentativa}`);
-        break;
-    } else if (numeroEscolhido > numeroSecreto) {
-        alert(`Tente novamente! O número secreto é menor que ${numeroEscolhido}.`);
-    } else {
-        alert(`Tente novamente! O número secreto é maior que ${numeroEscolhido}`);
-    }
-    
-    chute++;
 }
+
+reiniciar();
